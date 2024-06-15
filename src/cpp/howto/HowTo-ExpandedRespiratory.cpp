f27e2030d38352420ad869a7cf3aee3210b3b869
JeffWebb6 <jeff.webb@kitware.com>
2024-06-15 08:52:58 -0400

    Updated expanded respiratory SDK example.

--- src/cpp/howto/HowTo-ExpandedRespiratory.cpp
+++ src/cpp/howto/HowTo-ExpandedRespiratory.cpp
@@ -10,7 +10,7 @@
 #include "cdm/system/physiology/SEBloodChemistrySystem.h"
 #include "cdm/system/physiology/SECardiovascularSystem.h"
 #include "cdm/system/physiology/SERespiratorySystem.h"
-#include "cdm/patient/actions/SEAcuteRespiratoryDistressSyndromeExacerbation.h"
+#include "cdm/patient/actions/SEPrimaryBlastLungInjury.h"
 #include "cdm/properties/SEScalar0To1.h"
 #include "cdm/properties/SEScalarFrequency.h"
 #include "cdm/properties/SEScalarMassPerVolume.h"
@@ -20,7 +20,6 @@
 #include "cdm/properties/SEScalarVolume.h"
 #include "cdm/properties/SEScalarVolumePerTime.h"
 #include "cdm/properties/SEScalar0To1.h"
-
 
 void HowToExpandedRespiratory()
 {
@@ -35,62 +34,86 @@ void HowToExpandedRespiratory()
   pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("HeartRate", FrequencyUnit::Per_min);
   pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("SystolicArterialPressure", PressureUnit::mmHg);
   pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("DiastolicArterialPressure", PressureUnit::mmHg);
+  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("MeanArterialPressure", PressureUnit::mmHg);
+  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("EndTidalCarbonDioxidePressure", PressureUnit::mmHg);
+  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("HorowitzIndex", PressureUnit::mmHg);
+  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("OxygenSaturation");
   pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("RespirationRate", FrequencyUnit::Per_min);
   pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("TidalVolume", VolumeUnit::mL);
   pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("TotalLungVolume", VolumeUnit::mL);
-  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("OxygenSaturation");
+  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("ArterialOxygenPressure", PressureUnit::mmHg);
+  pe->GetEngineTracker()->GetDataRequestManager().CreatePhysiologyDataRequest("ArterialCarbonDioxidePressure", PressureUnit::mmHg);
+  pe->GetEngineTracker()->GetDataRequestManager().CreateGasCompartmentDataRequest("LeftLungPulmonary", "Volume", VolumeUnit::mL);
+  pe->GetEngineTracker()->GetDataRequestManager().CreateGasCompartmentDataRequest("RightLungPulmonary", "Volume", VolumeUnit::L);
+  pe->GetEngineTracker()->GetDataRequestManager().SetResultsFilename("./test_results/howto/HowTo_ExpandedRespiratory.csv");
 
   SEPatientConfiguration pc;
   pc.SetPatientFile("./patients/StandardMale.json");
 
   pe->InitializeEngine(pc);
 
-  pe->GetLogger()->Info("Initial vitals");
+  // Get default data at time 0s from the engine
+  pe->AdvanceModelTime(0, TimeUnit::s);
+  pe->GetLogger()->Info("Default data retrieved");
+
+  pe->GetLogger()->Info("Initial data");
   pe->GetLogger()->Info(std::stringstream() << "Tidal Volume : " << pe->GetRespiratorySystem()->GetTidalVolume(VolumeUnit::mL) << VolumeUnit::mL);
-  pe->GetLogger()->Info(std::stringstream() << "Systolic Pressure : " << pe->GetCardiovascularSystem()->GetSystolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
-  pe->GetLogger()->Info(std::stringstream() << "Diastolic Pressure : " << pe->GetCardiovascularSystem()->GetDiastolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
   pe->GetLogger()->Info(std::stringstream() << "Heart Rate : " << pe->GetCardiovascularSystem()->GetHeartRate(FrequencyUnit::Per_min) << "bpm");
   pe->GetLogger()->Info(std::stringstream() << "Respiration Rate : " << pe->GetRespiratorySystem()->GetRespirationRate(FrequencyUnit::Per_min) << "bpm");
   pe->GetLogger()->Info(std::stringstream() << "Oxygen Saturation : " << pe->GetBloodChemistrySystem()->GetOxygenSaturation());
+  pe->GetLogger()->Info(std::stringstream() << "Mean Arterial Pressure : " << pe->GetCardiovascularSystem()->GetMeanArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+  pe->GetLogger()->Info(std::stringstream() << "Horowitz Index : " << pe->GetRespiratorySystem()->GetHorowitzIndex(PressureUnit::mmHg) << PressureUnit::mmHg);
+  pe->GetLogger()->Info(std::stringstream() << "Total Lung Volume : " << pe->GetRespiratorySystem()->GetTotalLungVolume(VolumeUnit::mL) << VolumeUnit::mL);
+  pe->GetLogger()->Info(std::stringstream() << "Arterial Oxygen Pressure : " << pe->GetBloodChemistrySystem()->GetArterialOxygenPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+  pe->GetLogger()->Info(std::stringstream() << "Arterial Carbon Dioxide Pressure : " << pe->GetBloodChemistrySystem()->GetArterialCarbonDioxidePressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+
+  // Apply the standalone UNDEX equation
+  pe->GetLogger()->Info("Charge information");
+  double chargeMass_lbs = 1000;
+  double chargeDistance_ft = 100; //635 ~= 0.1; 211 ~= 0.3; 70 ~= 0.9
+  double peakPressure = 13000 * pow(chargeMass_lbs, 0.33) / chargeDistance_ft;
+  double severity = 0.0005 * peakPressure;
+  severity = std::max(std::min(severity, 1.0), 0.0);
+
+  pe->GetLogger()->Info(std::stringstream() << "Charge Mass: " << chargeMass_lbs << " lbs");
+  pe->GetLogger()->Info(std::stringstream() << "Charge Distance: " << chargeDistance_ft << " ft");
+  pe->GetLogger()->Info(std::stringstream() << "Peak Pressure: " << peakPressure << " mmHg");
+  pe->GetLogger()->Info(std::stringstream() << "Severity: " << severity);
 
   pe->AdvanceModelTime(30, TimeUnit::s);
+  pe->GetLogger()->Info("Data after 30s");
 
-  pe->GetLogger()->Info("Healthy patient");
+  SEPrimaryBlastLungInjury pbli;
+  pbli.GetSeverity(eLungCompartment::RightSuperiorLobeApical).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightSuperiorLobePosterior).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightSuperiorLobeAnterior).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightMiddleLobeLateral).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightMiddleLobeMedial).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightInferiorLobeSuperior).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightInferiorLobeMedialBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightInferiorLobeAnteriorBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightInferiorLobeLateralBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::RightInferiorLobePosteriorBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftInferiorLobePosteriorBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftInferiorLobeLateralBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftInferiorLobeAnteromedialBasal).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftInferiorLobeSuperior).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftSuperiorLobeInferiorLingula).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftSuperiorLobeSuperiorLingula).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftSuperiorLobeAnterior).SetValue(severity);
+  pbli.GetSeverity(eLungCompartment::LeftSuperiorLobeApicoposterior).SetValue(severity);
+  pe->ProcessAction(pbli);
+
+  pe->AdvanceModelTime(15, TimeUnit::min);
+
+  pe->GetLogger()->Info("Data after 15 min");
   pe->GetLogger()->Info(std::stringstream() << "Tidal Volume : " << pe->GetRespiratorySystem()->GetTidalVolume(VolumeUnit::mL) << VolumeUnit::mL);
-  pe->GetLogger()->Info(std::stringstream() << "Systolic Pressure : " << pe->GetCardiovascularSystem()->GetSystolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
-  pe->GetLogger()->Info(std::stringstream() << "Diastolic Pressure : " << pe->GetCardiovascularSystem()->GetDiastolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
   pe->GetLogger()->Info(std::stringstream() << "Heart Rate : " << pe->GetCardiovascularSystem()->GetHeartRate(FrequencyUnit::Per_min) << "bpm");
   pe->GetLogger()->Info(std::stringstream() << "Respiration Rate : " << pe->GetRespiratorySystem()->GetRespirationRate(FrequencyUnit::Per_min) << "bpm");
   pe->GetLogger()->Info(std::stringstream() << "Oxygen Saturation : " << pe->GetBloodChemistrySystem()->GetOxygenSaturation());
-
-  SEAcuteRespiratoryDistressSyndromeExacerbation ARDS;
-  ARDS.GetSeverity(eLungCompartment::RightSuperiorLobeApical).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightSuperiorLobePosterior).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightSuperiorLobeAnterior).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightMiddleLobeLateral).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightMiddleLobeMedial).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightInferiorLobeSuperior).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightInferiorLobeMedialBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightInferiorLobeAnteriorBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightInferiorLobeLateralBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::RightInferiorLobePosteriorBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftInferiorLobePosteriorBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftInferiorLobeLateralBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftInferiorLobeAnteromedialBasal).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftInferiorLobeSuperior).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftSuperiorLobeInferiorLingula).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftSuperiorLobeSuperiorLingula).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftSuperiorLobeAnterior).SetValue(0.3);
-  ARDS.GetSeverity(eLungCompartment::LeftSuperiorLobeApicoposterior).SetValue(0.3);
-  pe->ProcessAction(ARDS);
-
-  pe->AdvanceModelTime(300, TimeUnit::s);
-
-  pe->GetLogger()->Info("Moderate ARDS");
-  pe->GetLogger()->Info(std::stringstream() << "Tidal Volume : " << pe->GetRespiratorySystem()->GetTidalVolume(VolumeUnit::mL) << VolumeUnit::mL);
-  pe->GetLogger()->Info(std::stringstream() << "Systolic Pressure : " << pe->GetCardiovascularSystem()->GetSystolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
-  pe->GetLogger()->Info(std::stringstream() << "Diastolic Pressure : " << pe->GetCardiovascularSystem()->GetDiastolicArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
-  pe->GetLogger()->Info(std::stringstream() << "Heart Rate : " << pe->GetCardiovascularSystem()->GetHeartRate(FrequencyUnit::Per_min) << "bpm");
-  pe->GetLogger()->Info(std::stringstream() << "Respiration Rate : " << pe->GetRespiratorySystem()->GetRespirationRate(FrequencyUnit::Per_min) << "bpm");
-  pe->GetLogger()->Info(std::stringstream() << "Oxygen Saturation : " << pe->GetBloodChemistrySystem()->GetOxygenSaturation());
+  pe->GetLogger()->Info(std::stringstream() << "Mean Arterial Pressure : " << pe->GetCardiovascularSystem()->GetMeanArterialPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+  pe->GetLogger()->Info(std::stringstream() << "Horowitz Index : " << pe->GetRespiratorySystem()->GetHorowitzIndex(PressureUnit::mmHg) << PressureUnit::mmHg);
+  pe->GetLogger()->Info(std::stringstream() << "Total Lung Volume : " << pe->GetRespiratorySystem()->GetTotalLungVolume(VolumeUnit::mL) << VolumeUnit::mL);
+  pe->GetLogger()->Info(std::stringstream() << "Arterial Oxygen Pressure : " << pe->GetBloodChemistrySystem()->GetArterialOxygenPressure(PressureUnit::mmHg) << PressureUnit::mmHg);
+  pe->GetLogger()->Info(std::stringstream() << "Arterial Carbon Dioxide Pressure : " << pe->GetBloodChemistrySystem()->GetArterialCarbonDioxidePressure(PressureUnit::mmHg) << PressureUnit::mmHg);
 }
--- src/python/pulse/howto/HowTo_ExpandedRespiratory.py
+++ src/python/pulse/howto/HowTo_ExpandedRespiratory.py
@@ -26,17 +26,17 @@ def HowTo_ExpandedRespiratory():
 
     data_requests = [
         SEDataRequest.create_physiology_request("HeartRate", unit=FrequencyUnit.Per_min),
-        SEDataRequest.create_physiology_request("ArterialPressure", unit=PressureUnit.mmHg),
-        SEDataRequest.create_physiology_request("MeanArterialPressure", unit=PressureUnit.mmHg),
         SEDataRequest.create_physiology_request("SystolicArterialPressure", unit=PressureUnit.mmHg),
         SEDataRequest.create_physiology_request("DiastolicArterialPressure", unit=PressureUnit.mmHg),
-
+        SEDataRequest.create_physiology_request("MeanArterialPressure", unit=PressureUnit.mmHg),
         SEDataRequest.create_physiology_request("EndTidalCarbonDioxidePressure", unit=PressureUnit.mmHg),
         SEDataRequest.create_physiology_request("HorowitzIndex", unit=PressureUnit.mmHg),
         SEDataRequest.create_physiology_request("OxygenSaturation"),
         SEDataRequest.create_physiology_request("RespirationRate", unit=FrequencyUnit.Per_min),
         SEDataRequest.create_physiology_request("TidalVolume", unit=VolumeUnit.mL),
         SEDataRequest.create_physiology_request("TotalLungVolume", unit=VolumeUnit.mL),
+        SEDataRequest.create_physiology_request("ArterialOxygenPressure", unit=PressureUnit.mmHg),
+        SEDataRequest.create_physiology_request("ArterialCarbonDioxidePressure", unit=PressureUnit.mmHg),
         SEDataRequest.create_gas_compartment_request("LeftLungPulmonary", "Volume", unit=VolumeUnit.mL),
         SEDataRequest.create_gas_compartment_request("RightLungPulmonary", "Volume", unit=VolumeUnit.L),
     ]
@@ -68,6 +68,21 @@ def HowTo_ExpandedRespiratory():
     results = pulse.pull_data()
     pulse.print_results()
 
+    # Apply the standalone UNDEX equation
+    # Inputs
+    chargeMass_lbs = 1000
+    chargeDistance_ft = 100 #635 ~= 0.1; 211 ~= 0.3; 70 ~= 0.9
+    # Pressure equation
+    peakPressure = 13000 * (chargeMass_lbs ** 0.33) / chargeDistance_ft    
+    # PBLI severity mapping
+    severity = 0.0005 * peakPressure
+    # Bounding the severity between 0 and 1
+    severity = max(min(severity, 1.0), 0.0)
+
+    print(f"Charge Mass: {chargeMass_lbs} lbs")
+    print(f"Charge Distance: {chargeDistance_ft} ft")
+    print(f"Peak Pressure: {peakPressure} mmHg")
+    print(f"Severity: {severity}")
 
     # Advance some time and print out the vitals
     pulse.advance_time_s(30)
@@ -75,28 +90,28 @@ def HowTo_ExpandedRespiratory():
     pulse.print_results()
 
     pbli = SEPrimaryBlastLungInjury()
-    pbli.get_severity(eLungCompartment.RightSuperiorLobeApical).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightSuperiorLobePosterior).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightSuperiorLobeAnterior).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightMiddleLobeLateral).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightMiddleLobeMedial).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightInferiorLobeSuperior).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightInferiorLobeMedialBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightInferiorLobeAnteriorBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightInferiorLobeLateralBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.RightInferiorLobePosteriorBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftInferiorLobePosteriorBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftInferiorLobeLateralBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftInferiorLobeAnteromedialBasal).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftInferiorLobeSuperior).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftSuperiorLobeInferiorLingula).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftSuperiorLobeSuperiorLingula).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftSuperiorLobeAnterior).set_value(0.3)
-    pbli.get_severity(eLungCompartment.LeftSuperiorLobeApicoposterior).set_value(0.3)
+    pbli.get_severity(eLungCompartment.RightSuperiorLobeApical).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightSuperiorLobePosterior).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightSuperiorLobeAnterior).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightMiddleLobeLateral).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightMiddleLobeMedial).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightInferiorLobeSuperior).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightInferiorLobeMedialBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightInferiorLobeAnteriorBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightInferiorLobeLateralBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.RightInferiorLobePosteriorBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftInferiorLobePosteriorBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftInferiorLobeLateralBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftInferiorLobeAnteromedialBasal).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftInferiorLobeSuperior).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftSuperiorLobeInferiorLingula).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftSuperiorLobeSuperiorLingula).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftSuperiorLobeAnterior).set_value(severity)
+    pbli.get_severity(eLungCompartment.LeftSuperiorLobeApicoposterior).set_value(severity)
     pulse.process_action(pbli)
 
     # Advance some time and print out the vitals
-    pulse.advance_time_s(300)
+    pulse.advance_time_s(900)
     results = pulse.pull_data()
     pulse.print_results()
 
