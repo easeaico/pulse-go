d0543e5c690b90df2c95d01a4e4e008ea7a5c39e
JeffWebb6 <jeff.webb@kitware.com>
2024-08-23 12:41:21 -0400

    Update website documentation.

--- src/cpp/cdm/test/ActionTest.cpp
+++ src/cpp/cdm/test/ActionTest.cpp
@@ -121,7 +121,6 @@ void CommonDataModelTest::ActionTest(const std::string& rptDirectory)
   // Patient Actions //
   /////////////////////
 
-  //Aaron - Update this
   SEAcuteRespiratoryDistressSyndromeExacerbation ards;
   ards.GetSeverity(eLungCompartment::LeftLung).SetValue(0.3);
   ards.GetSeverity(eLungCompartment::RightLung).SetValue(0.2);
@@ -252,10 +251,9 @@ void CommonDataModelTest::ActionTest(const std::string& rptDirectory)
   intub.SetType(eIntubation_Type::Esophageal);
   TestAction<SEIntubation>(testSuite.CreateTestCase(), subMgr, intub, "-Severity-AirwayResistance-Type");
 
-  //Aaron -update this
   SEPneumoniaExacerbation lpe;
   lpe.GetSeverity(eLungCompartment::LeftLung).SetValue(0.3);
-  lpe.GetSeverity(eLungCompartment::LeftLung).SetValue(0.3);
+  lpe.GetSeverity(eLungCompartment::RightLung).SetValue(0.3);
   TestAction<SEPneumoniaExacerbation>(testSuite.CreateTestCase(), subMgr, lpe, "-Severity-LeftLungAffected-RightLungAffected");
 
   SEMechanicalVentilation mv;
