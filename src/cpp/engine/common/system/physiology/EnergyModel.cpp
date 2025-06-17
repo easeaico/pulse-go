96e0812fdb9f2a14b5024ff6eee71eb0ab60edff
JeffWebb6 <jeff.webb@kitware.com>
2025-06-17 10:55:11 -0400

    Fixed typo in male BMR calculation.

--- src/cpp/engine/common/system/physiology/EnergyModel.cpp
+++ src/cpp/engine/common/system/physiology/EnergyModel.cpp
@@ -749,7 +749,7 @@ namespace pulse
     /// \cite roza1984metabolic
     double patientBMR_kcal_Per_day = 0.0;
     if (m_data.GetCurrentPatient().GetSex() == ePatient_Sex::Male)
-      patientBMR_kcal_Per_day = 88.632 + 13.397 * PatientMass_kg + 4.799 * PatientHeight_cm - 5.677 * PatientAge_yr;
+      patientBMR_kcal_Per_day = 88.362 + 13.397 * PatientMass_kg + 4.799 * PatientHeight_cm - 5.677 * PatientAge_yr;
     else
       patientBMR_kcal_Per_day = 447.593 + 9.247 * PatientMass_kg + 3.098 * PatientHeight_cm - 4.330 * PatientAge_yr;
     // Systems do their math with MetabolicRate in Watts, so let's make these consistent
